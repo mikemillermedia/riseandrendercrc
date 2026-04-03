ec90746aad259dadda574bde9b1885c06553ed05
mikemillermedia <mike@mikemillermedia.com>
2026-04-03 13:21:55 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import Login from './Login';
+import Hub from './Hub';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* If they are at the main website URL, show the Landing Page */}
         <Route path="/" element={<LandingPage />} />
-        
-        {/* If they type /login, show the Login Page */}
         <Route path="/login" element={<Login />} />
+        <Route path="/hub" element={<Hub />} />
       </Routes>
     </BrowserRouter>
   );
