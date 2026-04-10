56cb19b0de30b88e0a447b0a15b2d0b25faf237a
mikemillermedia <mike@mikemillermedia.com>
2026-04-09 22:06:21 -0500

    Update FluidBackground.tsx

--- components/FluidBackground.tsx
+++ components/FluidBackground.tsx
@@ -103,7 +103,7 @@ const FluidBackground: React.FC = () => {
       />
 
       {/* Static Grain Overlay */}
-      <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-20 mix-blend-overlay pointer-events-none"></div>
+      <div className="absolute inset-0 bg-[url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")] opacity-20 mix-blend-overlay pointer-events-none"></div>
       
       {/* Vignette */}
       <div className="absolute inset-0 bg-radial-gradient from-transparent via-transparent to-black pointer-events-none" />
