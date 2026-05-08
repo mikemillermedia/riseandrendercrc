64abf5a901dcbbfeef1f4b086313830f72f294a7
mikemillermedia <mike@mikemillermedia.com>
2026-05-08 09:32:22 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import Login from './Login';
 import Hub from './Hub';
 import PricingPage from './PricingPage';
+import CookieConsent from './CookieConsent';
+import { loadAnalytics } from './analytics';
 
 export default function App() {
+  
+  // Check for returning users on initial load
+  useEffect(() => {
+    const userConsent = localStorage.getItem('user_cookie_consent');
+    if (userConsent === 'accepted_all') {
+      loadAnalytics();
+    }
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-<Route path="/pricing" element={<PricingPage />} />
-        <Route path="/hub" element={<Hub />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/pricing" element={<PricingPage />} />
+          <Route path="/hub" element={<Hub />} />
+        </Routes>
+      </BrowserRouter>
+      
+      {/* Global Cookie Consent Popup */}
+      <CookieConsent />
+    </>
   );
 }
