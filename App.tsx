4c620bf3341403db50f6ea7fa818456c53ea6196
mikemillermedia <mike@mikemillermedia.com>
2026-09-23 12:07:11 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,32 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import Login from './Login';
 import Hub from './Hub';
-import CookieConsent from './CookieConsent';
-import { loadAnalytics } from './analytics';
+// import CookieConsent from './CookieConsent';
+// import { loadAnalytics } from './analytics';
 
 export default function App() {
-  
-  // Check for returning users on initial load
-  useEffect(() => {
-    const userConsent = localStorage.getItem('user_cookie_consent');
-    if (userConsent === 'accepted_all') {
-      loadAnalytics();
-    }
-  }, []);
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          {/* THE MISSING ROUTE HAS BEEN ADDED BELOW */}
-          <Route path="/hub" element={<Hub />} />
-        </Routes>
-      </BrowserRouter>
-      
-      {/* Global Cookie Consent Popup */}
-      <CookieConsent />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/hub" element={<Hub />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
